4a3f92704e87030c00d0a81dd7d763e748b82957
ruangkosong33 <ruangkosong33@gmail.com>
2023-06-01 23:50:32 +0800

    landing1

---
+++ resources/views/landing/layouts/f-nav.blade.php
@@ -0,0 +1,144 @@
+<header class="main-header">
+    <nav class="main-menu">
+        <div class="main-menu__wrapper">
+            <div class="main-menu__wrapper-inner">
+                <div class="main-menu__logo">
+                    <a href="index.html"><img src="{{asset('rk99/assets/images/resources/logo-1.png')}}" alt=""></a>
+                </div>
+                <div class="main-menu__call">
+                    <div class="main-menu__call-icon">
+                        <span class="icon-telephone"></span>
+                    </div>
+                    <div class="main-menu__call-content">
+                        <p class="main-menu__call-sub-title">Call Anytime</p>
+                        <h5 class="main-menu__call-number"><a href="tel:928009850">+92 (8800)-9850</a></h5>
+                    </div>
+                </div>
+                <div class="main-menu__wrapper-inner-content">
+                    <div class="main-menu__top">
+                        <div class="main-menu__top-inner">
+                            <div class="main-menu__top-left">
+                                <ul class="list-unstyled main-menu__contact-list">
+                                    <li>
+                                        <div class="icon">
+                                            <i class="fas fa-envelope"></i>
+                                        </div>
+                                        <div class="text">
+                                            <p><a href="mailto:needhelp@company.com">needhelp@company.com</a>
+                                            </p>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="icon">
+                                            <i class="fas fa-map-marker"></i>
+                                        </div>
+                                        <div class="text">
+                                            <p>30 Broklyn Golden Street. New York</p>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="main-menu__top-right">
+                                <ul class="list-unstyled main-menu__top-menu">
+                                    <li><a href="about.html">About</a></li>
+                                    <li><a href="about.html">Help</a></li>
+                                    <li><a href="contact.html">Contact</a></li>
+                                </ul>
+                                <div class="main-menu__social">
+                                    <a href="#"><i class="fab fa-twitter"></i></a>
+                                    <a href="#"><i class="fab fa-facebook"></i></a>
+                                    <a href="#"><i class="fab fa-pinterest-p"></i></a>
+                                    <a href="#"><i class="fab fa-instagram"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="main-menu__bottom">
+                        <div class="main-menu__bottom-inner">
+                            <div class="main-menu__main-menu-box">
+                                <a href="#" class="mobile-nav__toggler"><i class="fa fa-bars"></i></a>
+                                <ul class="main-menu__list">
+                                    <li class="dropdown">
+                                        <a href="index.html">Home </a>
+                                        <ul>
+                                            <li><a href="index.html">Home One</a></li>
+                                            <li><a href="index2.html">Home Two</a></li>
+                                            <li><a href="index3.html">Home Three</a></li>
+                                            <li class="dropdown">
+                                                <a href="#">Header Styles</a>
+                                                <ul>
+                                                    <li><a href="index.html">Header One</a></li>
+                                                    <li><a href="index2.html">Header Two</a></li>
+                                                    <li><a href="index3.html">Header Three</a></li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li class="dropdown">
+                                        <a href="about.html">About</a>
+                                    </li>
+                                    <li class="dropdown">
+                                        <a href="#">Pages</a>
+                                        <ul class="sub-menu">
+                                            <li><a href="team.html">Our Team</a></li>
+                                            <li><a href="team-details.html">Team Details</a></li>
+                                            <li><a href="testimonials.html">Testimonials</a></li>
+                                            <li><a href="careers.html">Careers</a></li>
+                                            <li><a href="faq.html">Faqs</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="dropdown">
+                                        <a href="#">Services</a>
+                                        <ul class="sub-menu">
+                                            <li><a href="services.html">Services</a></li>
+                                            <li><a href="capital-market.html">Capital Market</a></li>
+                                            <li><a href="insurance.html">Insurance</a></li>
+                                            <li><a href="mutual-funds.html">Mutual Funds</a></li>
+                                            <li><a href="portfolio-mangement.html">Portfolio Mangement</a></li>
+                                            <li><a href="fixed-income.html">Fixed Income</a></li>
+                                            <li><a href="loans.html">Loans</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="dropdown">
+                                        <a href="#">Portfolio</a>
+                                        <ul class="sub-menu">
+                                            <li><a href="portfolio.html">Portfolio</a></li>
+                                            <li><a href="portfolio-details.html">Portfolio Details</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="dropdown">
+                                        <a href="#">News</a>
+                                        <ul class="sub-menu">
+                                            <li><a href="news.html">News</a></li>
+                                            <li><a href="news-details.html">News Details</a></li>
+                                        </ul>
+                                    </li>
+                                     <li class="dropdown">
+                                        <a href="#">Features</a>
+                                        <ul class="sub-menu">
+                                            <li><a href="sip.html">Sip Calculator</a></li>
+                                            <li><a href="inflation.html">Inflation Calculator</a></li>
+                                            <li><a href="targetamount-calculator.html">Target Calculator</a></li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="contact.html">Contact</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="main-menu__right">
+                                <div class="main-menu__search-box">
+                                    <a href="#"
+                                        class="main-menu__search search-toggler icon-magnifying-glass"></a>
+                                </div>
+                                <div class="main-menu__btn-box">
+                                    <a href="contact.html" class="thm-btn main-menu__btn">Free Consultation</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
