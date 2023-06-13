86b7641206d02ecd724c94987c8c68050e239578
anas misbah <anas.misbahuddin4869@gmail.com>
2023-06-13 16:45:27 +0800

    Cnt

--- app/Http/Controllers/Beranda/BerandaController.php
+++ app/Http/Controllers/Beranda/BerandaController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Beranda;
 
 use App\Models\Banner;
+use App\Models\Post;
+use App\Models\Gallery;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -11,9 +13,14 @@ class BerandaController extends Controller
     public function index()
     {
         $banners=Banner::latest()->get();
+        $latestGallery=Gallery::latest()->take(15)->get();
+        $latestPost=Post::latest()->take(3)->get();
+
 
         return view('landing.pages.beranda.index-beranda', [
             'banners'=>$banners,
+            'latestPost'=>$latestPost,
+            'latestGallery'=>$latestGallery,
         ]);
     }
 }
